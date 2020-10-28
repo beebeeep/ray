fd8850e09448695f168a28c2ba0561690e983c09
Danila Migalin <miga@uber.com>
2020-10-28 21:21:17 +0200

    added Phong lighting

--- main.go
+++ main.go
@@ -2,26 +2,52 @@ package main
 
 import (
 	"image"
-	"image/color"
+	_color "image/color"
 	"image/png"
 	"math"
 	"os"
 )
 
 const (
-	RESX = 1280
-	RESY = 1024
+	RESX = 3840
+	RESY = 2160
 )
 
 var (
-	matRed = material{color.NRGBA{0xd0, 0x30, 0x30, 0xff}}
-	matGreen = material{color.NRGBA{0x30, 0xd0, 0x30, 0xff}}
-	matOrange = material{color.NRGBA{0xd0, 0x90, 0x20, 0xff}}
+	matIvory = material{vector{0.24, 0.24, 0.18}, vector{0.3, 0.3, 0.3}, vector{0, 0, 0}, 50}
+	matRedMetal = material{vector{0.37, 0.13, 0.13}, vector{0.06, 0.03, 0.03}, vector{0, 0, 0}, 10}
+	matOrangeMetal = material{vector{0.45, 0.25, 0.1}, vector{0.03, 0.02, 0.01}, vector{0, 0, 0}, 10}
+
+	backgroundColor = vector{0.2, 0.2, 0.2}
 )
 
 type vector struct {
 	x, y, z float64
 }
+
+type material struct {
+	diffuse, specular, ambient vector
+	specularExp float64
+}
+
+type light struct {
+	position vector
+	intensity float64
+}
+
+type object interface {
+	rayIntersect(origin, dir vector) (float64, bool)
+	getMaterial() material
+	getNormale(p vector) vector
+}
+
+type sphere struct {
+	radius float64
+	center vector
+	material material
+}
+
+
 
 func NewNormalized(x, y, z float64) vector {
 	l := math.Sqrt(x*x + y*y + z*z)
@@ -48,34 +74,15 @@ func (v vector) Normalize() vector {
 	return NewNormalized(v.x, v.y, v.z)
 }
 
-type material struct {
-	diffuseColor color.NRGBA
+func (v vector) Reflect(n vector) vector {
+	// assuming v is normalized
+	return v.Sub(n.Multiply(2.0*v.DotProduct(n)))
 }
 
-type sphere struct {
-	radius float64
-	center vector
-	material material
-}
-
-type light struct {
-	position vector
-	intensity float64
-}
-
-type object interface {
-	rayIntersect(origin, dir vector) (float64, bool)
-	getMaterial() material
-	getNormale(p vector) vector
-}
-
-func colorMultiply(c color.NRGBA, i float64) color.NRGBA {
-	if i < 0 {
-		return color.NRGBA{}
-	}
-	R := float64(c.R) * i
-	G := float64(c.G) * i
-	B := float64(c.B) * i
+func (v vector) toRGBA() _color.NRGBA {
+	R := v.x * 255
+	G := v.y * 255
+	B := v.z * 255
 	if R > 255 {
 		R = 255
 	}
@@ -85,8 +92,7 @@ func colorMultiply(c color.NRGBA, i float64) color.NRGBA {
 	if B > 255 {
 		B = 255
 	}
-
-	return color.NRGBA{uint8(R), uint8(G), uint8(B), c.A}
+	return _color.NRGBA{uint8(R), uint8(G), uint8(B), 255}
 }
 
 func (s sphere) getMaterial() material {
@@ -110,15 +116,13 @@ func (s sphere) rayIntersect(origin, dir vector) (float64, bool) {
 	}
 
 	di := math.Sqrt(float64(s.radius*s.radius - d2)) // distance between 1st intersection and projection of center to the ray
-	t0 := plm - di
-	t1 := plm + di
-	if t0 < 0 {
-		t0 = t1
+	if r := plm - di; r >= 0 {
+		return r, true
 	}
-	if t0 < 0 {
-		return 0, false
+	if r := plm + di; r >= 0 {
+		return r, true
 	}
-	return t0, true
+	return 0, false
 }
 
 func sceneIntersec(origin, dir vector, scene []object) (intersection, normale vector, obj object) {
@@ -136,17 +140,29 @@ func sceneIntersec(origin, dir vector, scene []object) (intersection, normale ve
 	return intersection, normale, obj
 }
 
-func castRay(origin, dir vector, scene []object, lights []light) color.NRGBA {
+func castRay(origin, dir vector, scene []object, lights []light) vector {
 	intersection, normale, obj := sceneIntersec(origin, dir, scene)
 	if obj == nil {
-		return color.NRGBA{0x40, 0x40, 0x40, 0xff}
+		return backgroundColor
 	}
-	var diffuseLightIntensity float64
+
+	// calculate each factor for current intersection
+	var diffuseIntensity, specularIntensity vector
+	var diffuseFactor, specularFactor float64
 	for i := range lights {
 		lightDir := lights[i].position.Sub(intersection).Normalize()
-		diffuseLightIntensity += lights[i].intensity*math.Max(0, lightDir.DotProduct(normale))
+		diffuseFactor = lights[i].intensity*math.Max(0.0, lightDir.DotProduct(normale))
+		specularFactor = lights[i].intensity*math.Pow(
+			math.Max(0.0, lightDir.Reflect(normale).DotProduct(dir)),
+			obj.getMaterial().specularExp,
+		)
+
+		m := obj.getMaterial()
+		diffuseIntensity = diffuseIntensity.Add(m.diffuse.Multiply(diffuseFactor))
+		specularIntensity = specularIntensity.Add(m.diffuse.Multiply(specularFactor))
 	}
-	return colorMultiply(obj.getMaterial().diffuseColor, diffuseLightIntensity)
+
+	return specularIntensity.Add(diffuseIntensity)
 }
 
 func render(img *image.RGBA) {
@@ -154,13 +170,13 @@ func render(img *image.RGBA) {
 	ft := math.Tan(fov/2.0)
 	camera := vector{0, 0, 50}
 	scene := []object{
-		&sphere{5, vector{2, 2, -20}, matRed},
-		&sphere{2, vector{-5, 10, -30}, matGreen},
-		&sphere{8, vector{13, 5, -20}, matOrange},
+		&sphere{5, vector{2, 2, -20}, matOrangeMetal},
+		&sphere{2, vector{-5, 10, -30}, matIvory},
+		&sphere{8, vector{13, 5, -20}, matRedMetal},
 	}
 	lights := []light{
-		{vector{-10, 30, 10}, 1},
-		{vector{110, 0, 10}, 0.2},
+		{vector{-10, 30, 10}, 1.5},
+		{vector{110, 0, 10}, 0.7},
 	}
 
 	for y := 0; y < RESY; y++ {
@@ -168,7 +184,7 @@ func render(img *image.RGBA) {
 			dx := (2*(float64(x)+0.5)/float64(RESX) - 1.0) * ft * float64(RESX) / float64(RESY)
 			dy := -(2*(float64(y)+0.5)/float64(RESY) - 1.0) * ft
 			dir := NewNormalized(dx, dy, -1)
-			img.Set(x, y, castRay(camera, dir, scene, lights))
+			img.Set(x, y, castRay(camera, dir, scene, lights).toRGBA())
 		}
 	}
 }
