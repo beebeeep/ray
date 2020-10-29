fb3e554c373344b5181e9f42dfb69b588102ea6b
Danila Migalin <miga@uber.com>
2020-10-29 10:37:07 +0200

    updated scene

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ var (
 	matIvory = material{vector{0.24, 0.24, 0.18}, vector{0.03, 0.03, 0.03}, 50}
 	matRedMetal = material{vector{0.37, 0.13, 0.13}, vector{0.06, 0.03, 0.03},  10}
 	matOrangeMetal = material{vector{0.45, 0.25, 0.1}, vector{0.03, 0.02, 0.01}, 10}
-	matMirror = material{vector{0, 0, 0}, vector{0.8, 0.8, 0.8}, 1000}
+	matBlackGlossy = material{vector{0.04, 0.04, 0.04}, vector{0.15, 0.15, 0.15}, 50}
+	matMirror = material{vector{0, 0, 0}, vector{0.9, 0.9, 0.9}, 1000}
 
 	backgroundColor = vector{0.2, 0.2, 0.2}
 )
@@ -93,15 +94,15 @@ func castRay(origin, dir vector, scene []object, lights []light, depth int) vect
 }
 
 func render(img *image.RGBA) {
-	fov := math.Pi / 4.0
+	fov := math.Pi / 5.0
 	ft := math.Tan(fov/2.0)
-	camera := vector{0, 0, 50}
+	camera := vector{10, 5, 40}
 	scene := []object{
 		&sphere{5, vector{2, 2, -20}, matOrangeMetal},
 		&sphere{4, vector{-5, 10, -30}, matIvory},
 		&sphere{8, vector{13, 5, -20}, matRedMetal},
 		&sphere{1, vector{2, 5, -12}, matIvory},
-		&sphere{8, vector{28, 15, -12}, matMirror},
+		&sphere{8, vector{28, 15, -12}, matBlackGlossy},
 		&sphere{3, vector{7, -7, -18}, matMirror},
 	}
 	lights := []light{
